b0bfddd8b488dd0db65d05ffca56babd670478fd
KacperJagodzinski <kacper.jagodzinski@onet.eu>
2020-11-21 18:22:06 +0100

    Message app

---
+++ src/main/java/org/jagodzinski/chatsocket/ChatMessage.java
@@ -0,0 +1,21 @@
+package org.jagodzinski.chatsocket;
+
+public class ChatMessage {
+
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public ChatMessage(String value) {
+        this.value = value;
+    }
+
+    public ChatMessage() {
+    }
+}
